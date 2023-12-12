8162f9920e9b6c4b09adabecd335d6cedcf8762c
monil <user@Monil-MacBook.local>
2023-12-11 21:11:24 -0500

    add cors origin

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+const cors = require('cors');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,7 +23,7 @@ app.use(express.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("/uploads", express.static(__dirname + "/uploads"));
-
+app.use(cors());
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
