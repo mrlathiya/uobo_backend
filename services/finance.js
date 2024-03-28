3fc4c52a11f6d046b71e09c674cbd864e6133152
Monil Dumasia <52367664+VENDETTA-STACK@users.noreply.github.com>
2024-03-28 17:33:35 -0400

    add document

--- services/finance.js
+++ services/finance.js
@@ -356,7 +356,7 @@ module.exports = {
             status: params.status,
             appointments: params.appointments,
             billOfSale: params.billOfSale,
-            document: { $push: params.document },
+            $push: { document: params.document },
             additionalDocuments: params.additionalDocuments,
             customerSelectedCar: params.customerSelectedCar ? params.customerSelectedCar : undefined,
             dealerProvidedOptions: params.dealerProvidedOptions ? params.dealerProvidedOptions : undefined,
