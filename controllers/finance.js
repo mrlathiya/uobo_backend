3c246ab3a28fa46219cd4b5ac889d3dda1e613e8
Monil Dumasia <52367664+VENDETTA-STACK@users.noreply.github.com>
2024-08-04 18:09:32 -0400

    live order status change

--- controllers/finance.js
+++ controllers/finance.js
@@ -642,7 +642,7 @@ module.exports = {
                     Message: 'Customer requested orders found' 
                 });
             } else {
-                return res.status(400).json({ IsSuccess: false, Data: [], Message: 'Customer requested orders not found' });
+                return res.status(200).json({ IsSuccess: false, Data: [], Message: 'Customer requested orders not found' });
             }
         } catch (error) {
             return res.status(500).json({ IsSuccess: false, Message: error.message });
