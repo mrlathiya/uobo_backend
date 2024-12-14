f9a73dfe58398dbed1b5a78498737d8f474da830
Harsh Lathiya <62006931+mrlathiya@users.noreply.github.com>
2024-12-13 22:22:08 -0500

    Remove sensitive data

--- config/send-notification.js
+++ config/send-notification.js
@@ -1,7 +1,7 @@
 const { google } = require('googleapis');
 const request = require('request');
 const notificationSchema = require('../models/notificationStorage');
-const serviceAccount = require('../uobo-2e6e2-firebase-adminsdk-c3ubb-d1bfc0434d.json');
+const serviceAccount = require('../uobo-2e6e2-firebase-adminsdk-c3ubb-b2ddfb4630.json');
 
 const sendFirebaseNotification = async (token, title, body, data, category, senderId, receiverId, isSenderDealer) => {
   try {
